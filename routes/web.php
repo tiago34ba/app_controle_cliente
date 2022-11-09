ab2fc7a66aa78cf0c64556d80294dc45e4563867
tiago34ba <tiago2008.1@hotmail.com>
2022-11-08 21:44:22 -0300

    Atualização do sistema de gerenciamento de cliente

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('produtos', 'produtos\produtosController');
+
+    Route::resource('categorias', 'categorias\categoriasController');
+
+    Route::resource('vendas', 'vendas\vendasController');
+
+    Route::get('numero-vendas', 'vendas\vendasController@numeroVendas')->name('numero-vendas');
+
+    Route::get('numero-produtos', 'produtos\produtosController@numeroProdutos')->name('numero-produtos');
+
+    Route::get('numero-categorias', 'categorias\categoriasController@numeroCategorias')->name('numero-categorias');
+
+    Route::get('categorias-produtos', 'categorias\categoriasController@categoriaCadastraProduto')->name('categorias-produtos');
+
+    Route::get('relatorio-mensal', 'relatorios\relatoriosController@relatorioMensal')->name('relatorio-mensal');
+});
